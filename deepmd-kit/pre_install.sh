081885735635aedb1447c261aa50abc926ec83a9
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-09-13 03:00:17 -0400

    fix error in pre_install.sh

--- deepmd-kit/pre_install.sh
+++ deepmd-kit/pre_install.sh
@@ -1,6 +1,6 @@
 cat << EOF
 Notes:
-The off-line packages and conda packages require the GNU C Library 2.17 or above[1]. The GPU version requires compatible NVIDIA driver to be installed in advance[2]. It is possible to force conda to override detection when installation[3] (such as `CONDA_OVERRIDE_CUDA`), but these requirements are still necessary during runtime.
+The off-line packages and conda packages require the GNU C Library 2.17 or above[1]. The GPU version requires compatible NVIDIA driver to be installed in advance[2]. It is possible to force conda to override detection when installation[3] (such as CONDA_OVERRIDE_CUDA), but these requirements are still necessary during runtime.
 
 [1] The GNU C Library. https://www.gnu.org/software/libc/
 [2] Minor Version Compatibility. NVIDIA Data Center GPU Driver Documentation. https://docs.nvidia.com/deploy/cuda-compatibility/index.html#minor-version-compatibility
